145387d07bbc908ad514ade65f9249923faeb90f
shogo.sawada <shogo.sawada@rakus-partners.co.jp>
2019-07-12 17:18:58 +0900

    aaaaaa

--- src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
+++ src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
@@ -50,7 +50,7 @@ public class SampleAction {
 
         lastName = request.getParameter("LastName");
         if ((lastName == null) || ("".equals(lastName))) {
-            return false;
+            return true;
         }
 
         return true;
